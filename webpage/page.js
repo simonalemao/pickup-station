717a680f8f4b01ebd244b397664b7736d844791a
simon <59893147+simonalemao@users.noreply.github.com>
2020-12-08 21:33:33 +0100

    delete duplicate

--- webpage/page.js
+++
@@ -1,8 +0,0 @@
-async function testFunction() {
-   const fet = await fetch("/", {
-      headers: {
-         "x-user": "app"
-      }
-   });
-   document.getElementById("theDiv").innerHTML = JSON.stringify(await fet.json());
-}
